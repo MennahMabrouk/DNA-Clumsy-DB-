cf06224a51b316848030fce641cbac282d8ee319
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-01-01 09:49:20 +0200

    Update gallary.py

--- Project/gallary.py
+++ Project/gallary.py
@@ -28,21 +28,22 @@ def display_gallery():
         with col1:
             st.write(f"**{row['Name']}**")
 
-        if 'Image_Path' in row:
-            image_url = row['Image_Path']
+            if 'Image_Path' in row:
+                image_url = row['Image_Path']
 
-            response = requests.get(image_url)
-            if response.status_code == 200:
-                image = Image.open(BytesIO(response.content))
-                # Adjust the image size for the gallery-like view
-                st.image(image, caption=row['Name'], width=300, use_column_width=False)
-            else:
-                st.write(f"Failed to retrieve image for {row['Name']}")
+                response = requests.get(image_url)
+                if response.status_code == 200:
+                    image = Image.open(BytesIO(response.content))
+                    # Adjust the image size for the gallery-like view
+                    st.image(image, caption=row['Name'], width=300, use_column_width=False)
+                else:
+                    st.write(f"Failed to retrieve image for {row['Name']}")
 
-        # Add a click event to open a new page for each DNA entry
-        button_key = f"View {row['Name']}"
-        if col1.button(button_key):
-            display_dna_page(row)
+        with col2:
+            # Add a click event to open a new page for each DNA entry
+            button_key = f"View {row['Name']}"
+            if st.button(button_key):
+                display_dna_page(row)
 
 def display_dna_page(dna_entry):
     st.title(f"{dna_entry['Name']} Details")
@@ -57,8 +58,10 @@ def display_dna_page(dna_entry):
 
     data = pd.DataFrame({"x": x, "y": y})
 
+    # Display the plot in Streamlit
     st.line_chart(data.set_index("x"))
 
 # Run the app
 if __name__ == '__main__':
     display_gallery()
+
