9f4d886f81837e32a052df1457f02a60575b000c
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-01-01 06:45:21 +0200

    Update test.py

--- Project/test.py
+++ Project/test.py
@@ -21,32 +21,45 @@ st.markdown('<div class="image-container">', unsafe_allow_html=True)
 st.image("https://hms.harvard.edu/sites/default/files/media/DNA-850.jpg", width=None, caption="DNA Structure")
 st.markdown("</div>", unsafe_allow_html=True)
 
-# Render selected page
-if selected_page == "Sign Up":
+# User choice: Sign Up or Sign In
+choice = st.radio("", ["Sign Up", "Sign In"])
+
+# Sign-up section
+if choice == "Sign Up":
     st.subheader("Sign Up")
-    signup_username = st.text_input("Enter your username for signup", key="signup_username")
-    signup_password = st.text_input("Enter your password for signup", type="password", key="signup_password")
+    signup_username = st.text_input("Username for signup")
+    signup_password = st.text_input("Password for signup", type="password")
 
     # Additional details with improved styling
     with st.form("signup-form"):
         st.text("Additional Details")
-        user_type = st.selectbox("Select your user type:", ["Student", "Researcher", "Academic"])
-        phone_number = st.text_input("Enter your phone number:")
-        email = st.text_input("Enter your email:")
-        age = st.number_input("Enter your age:", min_value=0, max_value=150)
-        gender = st.radio("Select your gender:", ["Male", "Female", "Other"])
+        user_type = st.selectbox("User type:", ["Student", "Researcher", "Academic"])
+        phone_number = st.text_input("Phone number:")
+        email = st.text_input("Email:")
+        age = st.number_input("Age:", min_value=0, max_value=150)
+        gender = st.radio("Gender:", ["Male", "Female", "Other"])
 
         signup_button = st.form_submit_button("Sign Up")
 
-elif selected_page == "Sign In":
+    # Display gallery after sign up
+    if signup_button:
+        st.markdown("<h2>Welcome to the DNA Gallery!</h2>", unsafe_allow_html=True)
+        gene_gallery()
+
+# Sign-in section
+elif choice == "Sign In":
     st.subheader("Sign In")
 
     # Allow user to sign in by email or username
-    signin_identifier = st.text_input("Enter your email or username", key="signin_identifier")
-    signin_password = st.text_input("Enter your password for signin", type="password", key="signin_password")
+    signin_identifier = st.text_input("Email or username")
+    signin_password = st.text_input("Password for signin", type="password")
     signin_button = st.button("Sign In")
 
+    # Display gallery after sign in
     if signin_button:
         # Add your sign-in logic here
         # You can access the collected data like signin_identifier and signin_password
         st.success("Sign in successful!")
+        st.markdown("<h2>Welcome to the DNA Gallery!</h2>", unsafe_allow_html=True)
+        gene_gallery()
+
