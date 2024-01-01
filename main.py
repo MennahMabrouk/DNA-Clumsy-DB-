91fbdb158ad45e066be1f99e8124fd0681205326
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-01-01 06:54:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import streamlit as st
 from Project import signing
 from Project.theme_utils import set_theme
 
+# Set default theme
+set_theme("day")
+
 # Day and Night Theme Toggle
 if st.button("Toggle Theme"):
     current_theme = st.session_state.get("theme", "day")
@@ -24,7 +27,13 @@ if page == "Home":
     st.title("Helical Hues Haven")
     st.markdown('<div class="title-container"></div>', unsafe_allow_html=True)
 elif page == "signing":
-    # Call the test.py file in the project directory
+    # Call the show function directly from signing.py
+    signing.show()
+elif page == "Contact":
+    st.title("Contact Us")
+    st.write("Feel free to reach out to us for any inquiries or collaborations.")
+    # Add more content for the "Contact" page as needed
+
     signing.show()
 elif page == "Contact":
     st.title("Contact Us")
