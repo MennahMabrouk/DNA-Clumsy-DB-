f3cd63e35da5bff3daf82b27406756439ed070bc
MennahTullah Mabrouk <101124995+MennahMabrouk@users.noreply.github.com>
2024-01-01 08:55:09 +0200

    Update gallary.py

--- Project/gallary.py
+++ Project/gallary.py
@@ -6,11 +6,12 @@ from PIL import Image
 # Sample DNA data with direct image links
 dna_data = {
     'Name': ['DNA1', 'DNA2', 'DNA3'],
-    'Image_Path': [
-"image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwirecase3d.com%2Fproducts%2Fdna-3d-model&psig=AOvVaw0hqp_y50IY2MsJs68m95ST&ust=1704177903344000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNippNrLu4MDFQAAAAAdAAAAABAD"
-"image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fimages%2Fdna-3d-dna-strands%2F82359905&psig=AOvVaw0hqp_y50IY2MsJs68m95ST&ust=1704177903344000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNippNrLu4MDFQAAAAAdAAAAABAP"
-"image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2F3d-dna&psig=AOvVaw0hqp_y50IY2MsJs68m95ST&ust=1704177903344000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNippNrLu4MDFQAAAAAdAAAAABAV"
-    ],
+'Image_Path': [
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwirecase3d.com%2Fproducts%2Fdna-3d-model&psig=AOvVaw0hqp_y50IY2MsJs68m95ST&ust=1704177903344000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNippNrLu4MDFQAAAAAdAAAAABAD',
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fimages%2Fdna-3d-dna-strands%2F82359905&psig=AOvVaw0hqp_y50IY2MsJs68m95ST&ust=1704177903344000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNippNrLu4MDFQAAAAAdAAAAABAP',
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2F3d-dna&psig=AOvVaw0hqp_y50IY2MsJs68m95ST&ust=1704177903344000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNippNrLu4MDFQAAAAAdAAAAABAV'
+],
+
     'Sequence': ['ATCGATCG', 'GCTAGCTA', 'TAGCTAGC']
 }
 
